94ecfa9edda817fb390d7ac5f8f26666438dff64
Daniel Chincoya <chincoya@ciencias.unam.mx>
2020-09-24 16:29:27 -0500

    configure linter

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   context 'Valid Factory' do
     it 'has valid factory' do
       expect(build(:user).valid?).to be_truthy
@@ -15,27 +14,27 @@ RSpec.describe User, type: :model do
       user = build(:user, email: nil, password: nil)
       expect(user.valid?).to be_falsy
     end
-  
+
     it 'is invalid without email' do
       user = build(:user, email: nil, password: 'password')
       expect(user.valid?).to be_falsy
     end
-  
+
     it 'is invalid without password' do
       user = build(:user, email: Faker::Internet.email, password: nil)
       expect(user.valid?).to be_falsy
     end
-  
+
     it 'is valid with correct parameters' do
       user = build(:user)
       expect(user.valid?).to be_truthy
     end
-  
+
     it 'has unique email' do
       user1 = build(:user, email: 'example@example.com', password: 'password')
       user2 = build(:user, email: 'example@example.com', password: 'password')
       user1.save
-  
+
       expect(user2.save).to be_falsy
     end
   end
@@ -53,11 +52,10 @@ RSpec.describe User, type: :model do
 
     it 'can not be registered with existing email' do
       @user.save
-      new_user = User.new({email: @user.email, password: 'password'})
+      new_user = User.new({ email: @user.email, password: 'password' })
 
       expect(new_user.save).to be_falsy
       expect(User.all.length).to eql(1)
     end
   end
-  
 end
