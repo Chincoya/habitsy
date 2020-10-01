b2a086eaf75d91184573d581c55cf1624e347842
Daniel Chincoya <chincoya@ciencias.unam.mx>
2020-10-01 00:51:13 -0500

    Add some shared views and modified layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
 
     'HABITSY'
   end
+
+  def action_button_helper(button)
+    return '<div class="action-button"></div>'.html_safe if button.blank?
+
+    button
+  end
+
+  def date_helper(created_at)
+    created_at.strftime('%d %b %y')
+  end
+
+  def to_time_format(amount)
+    time = Time.at(amount * 60).utc.strftime('%HH:%MM')
+
+    "<span class=\"time\">#{time}</span>".html_safe
+  end
 end
